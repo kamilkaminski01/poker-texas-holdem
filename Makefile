530f54554b80354290a7e36eb2f34dc79a12f890
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-06-27 15:20:36 +0200

    Setup static code checks and linting tools

---
+++ Makefile
@@ -0,0 +1,45 @@
+.PHONY: build run recreate superuser check frontcheck lint pytest migrations migrate clear
+
+build:
+	docker compose build
+
+run:
+	docker compose up
+
+recreate:
+	docker compose up --build --force-recreate
+
+superuser:
+	docker compose run backend python manage.py createsuperuser
+
+check:
+	docker compose run --rm backend isort --check-only .
+	docker compose run --rm backend black --check .
+	docker compose run --rm backend flake8 .
+	docker compose run --rm backend mypy .
+
+frontcheck:
+	docker compose run --rm -T frontend npm run check
+
+lint:
+	docker compose run --rm -T backend isort .
+	docker compose run --rm -T backend black .
+	docker compose run --rm -T backend flake8 .
+	docker compose run --rm -T backend mypy .
+
+pytest:
+	docker compose run --rm backend pytest
+
+pytest_module:
+	docker compose run --rm backend pytest $(module)/
+
+migrations:
+	docker compose run backend python manage.py makemigrations
+
+migrate:
+	docker compose run backend python manage.py migrate
+
+clear:
+	docker compose down -v
+	docker system prune --force
+	docker volume prune --force
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,22 +14,22 @@ DEBUG = env.bool("DEBUG")
 ALLOWED_HOSTS = ["." + host.strip() for host in env.list("ALLOWED_HOSTS")]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
@@ -39,26 +39,26 @@ if DEBUG:
     MIDDLEWARE.insert(0, "corsheaders.middleware.CorsMiddleware")
     CORS_ORIGIN_ALLOW_ALL = True
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = "backend.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [os.path.join(BASE_DIR, "templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
 ASGI_APPLICATION = "backend.asgi.application"
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
@@ -110,18 +110,18 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
