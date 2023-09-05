caf54d9a10b3a62ed8c65de8b71091f83592ae3c
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-09-05 22:02:01 +0200

    Added mixins and fixed FloatingIcons styling

--- frontend/src/pages/AboutPage/partials/components/FloatingIcons/index.tsx
+++ frontend/src/pages/AboutPage/partials/components/FloatingIcons/index.tsx
@@ -10,15 +10,15 @@ import Docker from 'assets/icons/docker.png'
 import AWS from 'assets/icons/aws.png'
 
 const icons = [
-  { name: 'python', left: '20%', top: '20%', animationDelay: '0s', icon: Python },
-  { name: 'django', left: '45%', top: '25%', animationDelay: '0.3s', icon: Django },
-  { name: 'typescript', left: '15%', top: '55%', animationDelay: '1.7s', icon: TypeScript },
-  { name: 'react', left: '65%', top: '30%', animationDelay: '1s', icon: React },
-  { name: 'sass', left: '32%', top: '42%', animationDelay: '0.2s', icon: Sass },
-  { name: 'postgres', left: '50%', top: '55%', animationDelay: '1.3s', icon: Postgres },
-  { name: 'redis', left: '35%', top: '75%', animationDelay: '0.4s', icon: Redis },
-  { name: 'docker', left: '75%', top: '60%', animationDelay: '1.3s', icon: Docker },
-  { name: 'aws', left: '60%', top: '85%', animationDelay: '0.6s', icon: AWS }
+  { name: 'python', left: '15%', top: '15%', animationDelay: '0s', icon: Python },
+  { name: 'django', left: '40%', top: '20%', animationDelay: '0.3s', icon: Django },
+  { name: 'typescript', left: '10%', top: '50%', animationDelay: '1.7s', icon: TypeScript },
+  { name: 'react', left: '65%', top: '20%', animationDelay: '1s', icon: React },
+  { name: 'sass', left: '30%', top: '40%', animationDelay: '0.2s', icon: Sass },
+  { name: 'postgres', left: '50%', top: '50%', animationDelay: '1.3s', icon: Postgres },
+  { name: 'redis', left: '30%', top: '70%', animationDelay: '0.4s', icon: Redis },
+  { name: 'docker', left: '70%', top: '55%', animationDelay: '1.3s', icon: Docker },
+  { name: 'aws', left: '55%', top: '75%', animationDelay: '0.6s', icon: AWS }
 ]
 
 const FloatingIcons = () => {
